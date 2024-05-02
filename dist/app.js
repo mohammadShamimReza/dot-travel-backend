a6143d55118f150c14541d063ad827298da01e96
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2024-05-03 02:49:31 +0600

    remove /api/v1

--- dist/app.js
+++ dist/app.js
@@ -12,7 +12,7 @@ const app = (0, express_1.default)();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 
     // 'http://localhost:3000',
-    'https://dot-travel-frontend.vercel.app/api/v1');
+    'https://dot-travel-frontend.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
